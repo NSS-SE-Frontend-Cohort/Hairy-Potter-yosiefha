3e1b89b19b37889d8862360ee430578939658bfb
yosiefha <joehailem1@gmail.com>
2024-11-21 08:47:12 -0600

    Updated PetteryWheel

--- src/scripts/PotteryWheel.js
+++ src/scripts/PotteryWheel.js
@@ -1,17 +1,9 @@
 //Define a variable to have the value of the primary key for each piece of pottery. 
 let potteryId = 1;
 // Define and export a function named makePottery
-export const makePottery = (shape,weight,height)=>{
-
-    const pottery = {
-        shape: shape,
-        weight: weight,
-        height: height,
-        id: potteryId 
-    };
-
-    potteryId++; 
-
-    return pottery;
-
-}
+export const makePottery = (shape, weight, height) => ({
+    shape,
+    weight,
+    height,
+    id: potteryId++ // Increment potteryId inline
+});
